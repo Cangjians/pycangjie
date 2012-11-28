ffd2f6f9e0ce435ed45bde710864f5a28b9a5e0c
Mathieu Bridon <bochecha@fedoraproject.org>
2012-11-28 20:57:36 +0800

    Use proper relative imports
    This will help porting to Python 3.

--- src/cangjie/__init__.py
+++ src/cangjie/__init__.py
@@ -1,7 +1,7 @@
 # The stuff in core should be exposed directly here, but it seems to be
 # impossible to have a __init__.so file
-from _core import *
+from ._core import *
 
 # Native extentions don't get exposed automatically
-import languages
-import versions
+from . import languages
+from . import versions
