16a6c957113d9f916a5577be4facedaaddbe91c9
Mathieu Bridon <bochecha@fedoraproject.org>
2013-09-14 16:47:26 +0800

    errors: New module for error handling
    This will allow us to raise Python exceptions in result of the integer
    return codes from libcangjie2.

---
+++ src/cangjie/errors.pyx
@@ -0,0 +1,45 @@
+# Copyright (c) 2013 - The pycangjie authors
+#
+# This file is part of pycangjie, the Python bindings to libcangjie.
+#
+# pycangjie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pycangjie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pycangjie.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class CangjieError(Exception):
+    def __init__(self, *args):
+        if args:
+            super(CangjieError, self).__init__(*args)
+
+        else:
+            super(CangjieError, self).__init__(self.message)
+
+
+class CangjieNoCharsError(CangjieError):
+    message = "No characters found"
+
+
+class CangjieDBOpenError(CangjieError):
+    message = "Error opening the database"
+
+
+class CangjieDBError(CangjieError):
+    message = "Error querying the database"
+
+
+class CangjieMemoryError(CangjieError):
+    message = "Not enough memory"
+
+
+class CangjieInvalidInputError(CangjieError):
+    message = "Invalid input"
