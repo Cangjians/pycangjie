770b980d9fbb47a0330ee5034e471fb09370fe7f
Mathieu Bridon <bochecha@fedoraproject.org>
2013-09-14 18:33:30 +0800

    core: Drop stray comment

--- src/cangjie/_core.pyx
+++ src/cangjie/_core.pyx
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with pycangjie.  If not, see <http://www.gnu.org/licenses/>.
 
-#from cython.operator cimport preincrement as inc, dereference as deref
 
 cimport _core
 
